a4d63545140b97b2de5c17dd49ab0a262f550938
Raadwan Masum <piraadwan@gmail.com>
2020-08-31 03:51:46 -0700

    make script capable of running anywhere

--- sync.sh
+++ sync.sh
@@ -5,21 +5,21 @@ echo "syncing dotfiles..."
 cp ~/.zshrc .zshrc
 cp ~/.vimrc .vimrc
 cp ~/.tmux.conf .tmux.conf
-cp ~/.config/starship.toml ./config/starship.toml
-cp ~/.config/nvim/init.vim ./config/nvim/init.vim
-cp ~/.config/bspwm/bspwmrc ./config/bspwm/bspwmrc
-cp ~/.config/sxhkd/sxhkdrc ./config/sxhkd/sxhkdrc
-cp ~/.config/i3/config ./config/i3/config
-cp ~/.config/i3/i3blocks.conf ./config/i3/i3blocks.conf
-cp ~/.config/polybar/config ./config/polybar/config
-cp ~/.config/polybar/launch.sh ./config/polybar/launch.sh
-cp ~/.config/rofi/config.rasi ./config/rofi/config.rasi
-cp ~/.config/neofetch/config.conf ./config/neofetch/config.conf
-cp ~/.config/coc/extensions/package.json ./config/coc/extensions/package.json
-cp ~/.config/coc/memos.json ./config/coc/memos.json
+cp ~/.config/starship.toml ~/Github/dotfiles/config/starship.toml
+cp ~/.config/nvim/init.vim ~/Github/dotfiles/config/nvim/init.vim
+cp ~/.config/bspwm/bspwmrc ~/Github/dotfiles/config/bspwm/bspwmrc
+cp ~/.config/sxhkd/sxhkdrc ~/Github/dotfiles/config/sxhkd/sxhkdrc
+cp ~/.config/i3/config ~/Github/dotfiles/config/i3/config
+cp ~/.config/i3/i3blocks.conf ~/Github/dotfiles/config/i3/i3blocks.conf
+cp ~/.config/polybar/config ~/Github/dotfiles/config/polybar/config
+cp ~/.config/polybar/launch.sh ~/Github/dotfiles/config/polybar/launch.sh
+cp ~/.config/rofi/config.rasi ~/Github/dotfiles/config/rofi/config.rasi
+cp ~/.config/neofetch/config.conf ~/Github/dotfiles/config/neofetch/config.conf
+cp ~/.config/coc/extensions/package.json ~/Github/dotfiles/config/coc/extensions/package.json
+cp ~/.config/coc/memos.json ~/Github/dotfiles/config/coc/memos.json
 
 git add .
-git commit -a -m "automatically update dotfiles"
+git commit -a -m "Automatically Update Dotfiles"
 git push
 
 echo "dotfiles successfully synced!"
