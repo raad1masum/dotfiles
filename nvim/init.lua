b7a2de488b7b220a36eb7c40ecb15f2943d071dd
Raadwan Masum <rmasum@brex.com>
2026-02-24 12:02:45 -0800

    new neovim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,47 +1,44 @@
 require("config.lazy")
 
-vim.g.mapleader = " "
+-- Colorscheme
+vim.opt.termguicolors = true
+vim.opt.background = "dark"
+vim.cmd.colorscheme("gruvbox")
 
-vim.o.termguicolors = true
-vim.cmd[[colorscheme gruvbox]]
-vim.o.background = "dark"
-
-vim.o.number = true
-vim.o.relativenumber = true
-vim.o.expandtab = true
-vim.o.shiftwidth = 4
-vim.o.tabstop = 4
-vim.o.smartindent = true
-vim.o.mouse = "a"
-
-vim.opt.nu = true
+-- Line numbers
+vim.opt.number = true
 vim.opt.relativenumber = true
 
+-- Tabs & indentation
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-
 vim.opt.smartindent = true
 
+-- Line wrapping
 vim.opt.wrap = false
 
+-- File handling
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+local undodir = vim.fn.stdpath("state") .. "/undodir"
+if not (vim.uv or vim.loop).fs_stat(undodir) then
+    vim.fn.mkdir(undodir, "p")
+end
+vim.opt.undodir = undodir
 vim.opt.undofile = true
 
+-- Search
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
-vim.opt.termguicolors = true
-
+-- UI
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
-
--- vim.opt.updatetime = 50
-
-vim.g.mapleader = " "
-
+vim.opt.mouse = "a"
 vim.opt.clipboard = "unnamedplus"
+
+-- Keymaps
+vim.keymap.set("n", "<leader>w", "<cmd>w<cr>", { desc = "Save file" })
