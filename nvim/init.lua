b97f9d33a0005ca9e0688df5a4aa88bba4d7121e
Raadwan Masum <rmasum@brex.com>
2026-02-24 11:42:09 -0800

    add new neovim config

---
+++ nvim/init.lua
@@ -0,0 +1,47 @@
+require("config.lazy")
+
+vim.g.mapleader = " "
+
+vim.o.termguicolors = true
+vim.cmd[[colorscheme gruvbox]]
+vim.o.background = "dark"
+
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.expandtab = true
+vim.o.shiftwidth = 4
+vim.o.tabstop = 4
+vim.o.smartindent = true
+vim.o.mouse = "a"
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+-- vim.opt.updatetime = 50
+
+vim.g.mapleader = " "
+
+vim.opt.clipboard = "unnamedplus"
