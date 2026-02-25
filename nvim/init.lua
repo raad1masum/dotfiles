9c055d2f21c124db2428b60c1429648c68ace0e1
Raadwan Masum <rmasum@brex.com>
2026-02-25 15:13:12 -0800

    add leader q keymap for quit all

--- nvim/init.lua
+++ nvim/init.lua
@@ -42,6 +42,7 @@ vim.opt.clipboard = "unnamedplus"
 
 -- Keymaps
 vim.keymap.set("n", "<leader>w", "<cmd>w<cr>", { desc = "Save file" })
+vim.keymap.set("n", "<leader>q", "<cmd>qa<cr>", { desc = "Quit all" })
 vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left pane" })
 vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to lower pane" })
 vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to upper pane" })
